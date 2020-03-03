a59d404fc76325bd15fc136b161d8f04ab3a5ebb
patzalewska <61741083+patzalewska@users.noreply.github.com>
2020-03-03 09:52:26 -0300

    Exercício de temperatura
    Exercício do pré trabalho: conversão de temperatura

---
+++ app.js
@@ -0,0 +1,5 @@
+let temperature = parseInt(prompt("Qual a temperatura em Celcius hoje?"));
+let convertToFarenheit = (temperature*1.8) + 32;
+console.log("A temperatura em Farenheit é " + convertToFarenheit);
+
+document.write("A temperatura em Farenheit hoje é " + convertToFarenheit + ".")
